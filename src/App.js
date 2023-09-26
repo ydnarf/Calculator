34a0c7113032a7805723cda15da6e2774c039e0c
Frandy R. Nuñez <ydnarf13@hotmail.com>
2023-09-25 22:03:58 -0400

    Contenedor de la calculadora

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
     </div>
   );
 }
